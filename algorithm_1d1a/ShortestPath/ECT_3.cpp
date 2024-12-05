a3fe1c3b3db1bcd50e15f30ce6c23f0b2f82bf1f
yjr <110809579+YEJIRYOO@users.noreply.github.com>
2024-12-04 23:54:12 -0800

    1204

---
+++ algorithm_1d1a/ShortestPath/ECT_3.cpp
@@ -0,0 +1,56 @@
+#include <algorithm>
+#include <vector>
+#include <iostream>
+#define INF 1e9
+
+using namespace std;
+
+int n, m;
+int graph[501][501];
+
+int main(void) {
+    cin >> n >> m;
+
+    for (int i = 0; i < 501; i++) {
+        fill(graph[i], graph[i] + 501, INF);
+    }
+
+    //자기 자신에서 자기 자신으로 가는 비용은 0으로 초기화
+    for (int a = 1; a <= n; a++) {
+        for (int b = 1; b <= n; b++) {
+            if (a == b) graph[a][b] = 0;
+        }
+    }
+
+    //각 간선에 대한 정보를 입력 받아, 그 값으로 초기화
+    for (int i = 0; i < m; i++) {
+        // A에서 B로 가는 비용은 C라고 설정
+        int a, b, c;
+        cin >> a >> b >> c;
+        graph[a][b] = c;
+    }
+    
+    //점화식에 따라 플로이드 워셜 알고리즘을 수행
+    for (int k = 1; k <= n; k++) {
+        for (int a = 1; a <= n; a++) {
+            for (int b = 1; b <= n; b++) {
+                graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b]);
+            }
+        }
+    }
+
+    //수행된 결과를 출력
+    for (int a = 1; a <= n; a++) {
+        for (int b = 1; b <= n; b++) {
+            //도달할 수 없는 경우, 무한(INFINITY)이라고 출력
+            if (graph[a][b] == INF) {
+                cout << "INFINITY" << ' ';
+            }
+            //도달할 수 있는 경우 거리를 출력
+            else {
+                cout << graph[a][b] << ' ';
+            }
+        }
+        cout << '\n';
+    }
+}
